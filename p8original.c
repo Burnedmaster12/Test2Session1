0a520ed97e5fe2d5f4c7ba0f0bc72f57c05c9e4c
Faizal Hussain <96277916+Burnedmaster12@users.noreply.github.com>
2022-03-22 17:20:44 +0000

    done p8

--- p8original.c
+++ p8original.c
@@ -1,68 +1,94 @@
-#include <stdio.h>
-struct _point
-{
+#include<stdio.h>
+#include<math.h>
+struct _point {
 float x,y;
 };
-typedef struct _point Point;
+typedef struct _point point;
 struct _line
 {
-Point p1,p2;
+point p1,p2;
 float distance;
 };
-typedef struct _line Line;
-struct _polygon
+typedef struct _line line;
+struct _polygon 
 {
 int n;
-Line l[100];
+line l[100];
 float perimeter;
 };
-Line input_line()
-{
-  Line l;
-  printf("Enter the coordinates of the line");
-  scanf("%f %f", &l.p1.x,&l.p1.y);
-  printf("Enter the coordinates of the line");
-  scanf("%f %f", &l.p2.x,&l.p2.y);
-return l;
-}
 typedef struct _polygon Polygon;
-void input_polygon(Polygon *p)
+int input_n()
 {
+  int n;
+  printf("enter the number of sides of the polygon\n");
+  scanf("%d",&n);
+  return n;
+}
 
-  int i;
-  printf("Enter the number of sides");
-  scanf("%d",&p->n);
-  for(i=0;i<p->n;i++)
+float my_sqrt(float n)
+{
+  float z;
+  double sqrt =0,i,x;
+  i=n/2;
+  while (i<n){
+    x=(i + n/i)/2;
+    z=x;
+    if(z==i){
+      sqrt=z;
+      return sqrt;
+    }
+    else
+    i=z;
+    
+  }
+  return sqrt;
+}
+
+line input_line()
+{
+  point p1,p2;
+  line l1;
+  float f;
+  printf("enter the points p1\n");
+  scanf("%f%f",&p1.x,&p1.y);
+  printf("enter the points p2\n");
+  scanf("%f%f",&p2.x,&p2.y);
+  l1.p1=p1;
+  l1.p2=p2;
+  l1.distance=my_sqrt(((p2.x-p1.x)*(p2.x-p1.x))+((p2.y-p1.y)*(p2.y-p1.y)));
+  return l1;
+}
+void input_n_lines(int n, line l[n])
+{
+  for (int i=0;i<n;i++)
     {
-      p->l[i]=input_line();
+      l[i]=input_line();
     }
 }
-float find_distance(Point p1,Point p2)
+
+void input_polygon( int n, Polygon *p)
 {
-  return sqrt((p1.x-p2.x)*(p1.x-p2.x)+(p1.y-p2.y)*(p1.y-p2.y));
+  p->n=n;
+  input_n_lines(n,p->l);
 }
-void find_length(Line *l)
+void find_perimeter(int n,Polygon *p)
 {
-  l->distance=find_distance(l->p1,l->p2);
-}
-void find_perimeter(Polygon *p)
-{
-  int i;
   p->perimeter=0;
-  for(i=0;i<p->n;i++)
+  for (int i=0;i<n;i++)
     {
-      find_length(&p->l[i]);
-      p->perimeter+=p->l[i].distance;
+      p->perimeter=p->perimeter + p->l[i].distance;
     }
 }
-void output(Polygon p)
+void output(Polygon *p)
 {
-  printf("the perimeter of polygon if %f \n",p.perimeter);
+  printf("the perimeter of the polygon is as follows %f\n",p->perimeter);
 }
 int main()
 {
   Polygon p;
-  input_polygon(&p);
-  find_perimeter(&p);
-  output(p);
-  }
+  int n=input_n();
+  input_polygon(n,&p);
+  find_perimeter(n,&p);
+  output(&p);
+  return 0;
+}
